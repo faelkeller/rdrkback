dc9e37ec36fc191fb05bb32a4ae558c1def9f51f
Rafael Keller <rafaelkeller@sisconsultoria.com.br>
2020-05-27 23:51:21 -0300

    Get para menor preço de uma rota

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\QuoteRequest;
+use App\Route;
 
 class RouteController extends Controller
 {
 
     protected $quoteRequest;
+    protected $route;
 
-    public function __construct(
-        QuoteRequest $quoteRequest
-    )
-    {
+    public function __construct(QuoteRequest $quoteRequest, Route $route
+    ){
         $this->quoteRequest = $quoteRequest;
+        $this->route = $route;
     }
 
-    public function quote($start = "", $end = "")
-    {
+    public function quote($start = "", $end = ""){
         $this->quoteRequest->validate($start, $end);
 
-        return json_encode([]);
+        $quote = $this->route->quote($start, $end);
+
+        return json_encode($quote);
     }
 }
